9176045b78b077f3cf9976ae842dc57f96bf88e5
Toby Ladislas <tladislas@gmail.com>
2024-05-29 03:48:52 -0400

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const a: string = 'hi'
+console.log(a)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/index.ts'
+    ],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
