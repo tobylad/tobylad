a716ca888487dac358f7054b65e408a41c4975f1
Toby Lad <tobylad@Tobys-Air.attlocal.net>
2024-09-23 01:07:45 -0400

    Api key debugging

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
 
-const app = express();
-const port = process.env.PORT || 3000;
+dotenv.config()
 
-app.use(express.static(path.join(__dirname, 'build')));
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+const openWeatherApiKey = process.env.OPEN_WEATHER_API_KEY
+
+console.log(openWeatherApiKey, 'Sledgehammer')
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
