095f87ce864908ef5c59e7b55f587a8c4242cac1
Toby Lad <tobylad@Tobys-Air.attlocal.net>
2025-02-05 04:52:17 -0500

    Fixes portfolio bug; removes toy story.

--- src/SideProjects/SideProjects.tsx
+++ src/SideProjects/SideProjects.tsx
@@ -17,10 +17,6 @@ export const SideProjects: React.FC = () => {
                 <div className="site-cell">
                     <p key="paul-turbo"><a href="https://github.com/tobylad/paul-turbo" target="_blank">paul-turbo</a></p>
                 </div>
-
-                <div className="site-cell">
-                    <p key="toy-story-gpt"><Link to="/ask-toy-story">Ask Toy Story</Link></p>
-                </div>
             </div>
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,7 @@ import {
   Nav, 
   RetroWeather, 
   Blog,
-  SideProjects,
-  ToyStoryGpt
+  SideProjects
 } from './components'
 
 const container = document.getElementById('root')
@@ -25,7 +24,6 @@ if (container) {
             <Route path="retro-weather" element={<RetroWeather />} />
             <Route path="blog" element={<Blog />} />
             <Route path="side-projects" element={<SideProjects />} />
-            <Route path="ask-toy-story" element={<ToyStoryGpt />}> </Route>
           </Route>
         </Routes>
       </Router>
